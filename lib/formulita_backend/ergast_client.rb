899da536a843204850e93f268e1c2860f52ef31a
David Ortiz <z.david.ortiz@gmail.com>
2016-02-20 17:24:02 +0100

    ergast client: add methods to get races & results
    Also, clean-up the existing tests

--- lib/formulita_backend/ergast_client.rb
+++ lib/formulita_backend/ergast_client.rb
@@ -9,6 +9,18 @@ module FormulitaBackend
 
     class << self
 
+      def races(year)
+        response = get(races_path(year))
+        parsed_response = parse(response)
+        races_info(parsed_response).map do |race_info|
+          year = race_info['season'].to_i
+          round = race_info['round'].to_i
+          country = race_info['Circuit']['Location']['country']
+          end_date = race_info['date']
+          Race.new(year, round, country, end_date)
+        end
+      end
+
       # Drivers are returned in order according to their position in the
       # championship.
       def drivers(year)
@@ -35,6 +47,34 @@ module FormulitaBackend
         end
       end
 
+      def qualifying_results(year, round)
+        response = get(qualifying_results_path(year, round))
+        parsed_response = parse(response)
+        qualifying_info(parsed_response).map do |position_info|
+          position = position_info['position'].to_i
+          driver_code = position_info['Driver']['code']
+          q1 = position_info['Q1']
+          q2 = position_info['Q2']
+          q3 = position_info['Q3']
+          QualifyingResult.new(position, driver_code, q1, q2, q3)
+        end
+      end
+
+      def race_results(year, round)
+        response = get(race_results_path(year, round))
+        parsed_response = parse(response)
+        results_info(parsed_response).map do |position_info|
+          position = position_info['position'].to_i
+          driver_code = position_info['Driver']['code']
+          laps = position_info['laps'].to_i
+          status = position_info['status']
+          time = (status == 'Finished' ? position_info['Time']['time'] : nil)
+          grid = position_info['grid'].to_i
+          race_points = position_info['points'].to_i
+          RaceResult.new(position, driver_code, laps, time, status, grid, race_points)
+        end
+      end
+
       private
 
       def get(path)
@@ -43,6 +83,14 @@ module FormulitaBackend
 
       def parse(response)
         JSON.parse(response)
+      end
+
+      def races_path(year)
+        "#{BASE_PATH}#{year}/races.json".freeze
+      end
+
+      def races_info(races_parsed_resp)
+        races_parsed_resp['MRData']['RaceTable']['Races']
       end
 
       def drivers_path(year)
@@ -61,6 +109,24 @@ module FormulitaBackend
       def team_standings(teams_parsed_resp)
         teams_parsed_resp['MRData']['StandingsTable']['StandingsLists']
             .first['ConstructorStandings']
+      end
+
+      def qualifying_info(qualy_parsed_resp)
+        qualy_parsed_resp['MRData']['RaceTable']['Races']
+            .first['QualifyingResults']
+      end
+
+      def qualifying_results_path(year, round)
+        "#{BASE_PATH}#{year}/#{round}/qualifying.json"
+      end
+
+      def results_info(results_parsed_resp)
+        results_parsed_resp['MRData']['RaceTable']['Races']
+            .first['Results']
+      end
+
+      def race_results_path(year, round)
+        "#{BASE_PATH}#{year}/#{round}/results.json"
       end
 
     end
--- spec/ergast_client_spec.rb
+++ spec/ergast_client_spec.rb
@@ -2,13 +2,63 @@ require 'spec_helper'
 
 module FormulitaBackend
   describe ErgastClient do
+
+    # All the fixtures used in these tests have been simplified.
+    # For example, there are 19 races in the 2015 championship, but in order to
+    # simplify the tests, I use a fixture with only 2.
+
     subject { ErgastClient }
 
-    describe '.drivers' do
-      let(:year) { 2015 }
-      let(:url) { "http://ergast.com/api/f1/#{year}/driverstandings.json" }
+    let(:year) { 2015 }
+    let(:round) { 1 }
 
-      # Contains just 2 drivers for simplicity
+    # Objects that appear in the fixtures
+    let(:races) do
+      { australia: Race.new(2015, 1, 'Australia', '2015-03-15'),
+        malaysia: Race.new(2015, 2, 'Malaysia', '2015-03-29') }
+    end
+
+    let(:drivers) do
+      { hamilton: Driver.new('Lewis', 'Hamilton', 'British', 'Mercedes', 381),
+        rosberg: Driver.new('Nico', 'Rosberg', 'German', 'Mercedes', 322) }
+    end
+
+    let(:teams) do
+      { mercedes: Team.new('Mercedes', 'German', 703),
+        ferrari: Team.new('Ferrari', 'Italian', 428) }
+    end
+
+    let(:qualy_results) do
+      { hamilton_australia:
+            QualifyingResult.new(1, 'HAM', '1:28.586', '1:26.894', '1:26.327'),
+        rosberg_australia:
+            QualifyingResult.new(2, 'ROS', '1:28.906', '1:27.097', '1:26.921') }
+    end
+
+    let(:race_results) do
+      { hamilton_australia:
+            RaceResult.new(1, 'HAM', 58, '1:31:54.067', 'Finished', 1, 25),
+        rosberg_australia:
+            RaceResult.new(2, 'ROS', 58, '+1.360', 'Finished', 2, 18) }
+    end
+
+    describe '.races' do
+      let(:url) { "http://ergast.com/api/f1/#{year}/races.json" }
+      let(:races_fixture) { "#{fixtures_path}/races.json" }
+
+      before do
+        stub_request(:get, url)
+            .to_return(status: 200, body: IO.read(races_fixture))
+      end
+
+      it 'returns the races of the given season' do
+        expect(subject.races(year).map(&:to_h))
+            .to eq [races[:australia], races[:malaysia]].map(&:to_h)
+      end
+    end
+
+    describe '.drivers' do
+      let(:url) { "http://ergast.com/api/f1/#{year}/driverstandings.json" }
       let(:drivers_fixture) { "#{fixtures_path}/drivers.json" }
 
       before do
@@ -17,17 +67,13 @@ module FormulitaBackend
       end
 
       it 'returns the drivers of the given season' do
-        expect(subject.drivers(year).map(&:to_json))
-            .to eq [Driver.new('Lewis', 'Hamilton', 'British', 'Mercedes', 381),
-                    Driver.new('Nico', 'Rosberg', 'German', 'Mercedes', 322)].map(&:to_json)
+        expect(subject.drivers(year).map(&:to_h))
+            .to eq [drivers[:hamilton], drivers[:rosberg]].map(&:to_h)
       end
     end
 
     describe '.teams' do
-      let(:year) { 2015 }
       let(:url) { "http://ergast.com/api/f1/#{year}/constructorstandings.json" }
-
-      # Contains just 2 teams for simplicity
       let(:teams_fixture) { "#{fixtures_path}/teams.json" }
 
       before do
@@ -36,9 +82,40 @@ module FormulitaBackend
       end
 
       it 'returns the teams of the given season' do
-        expect(subject.teams(year).map(&:to_json))
-            .to eq [Team.new('Mercedes', 'German', 703),
-                    Team.new('Ferrari', 'Italian', 428)].map(&:to_json)
+        expect(subject.teams(year).map(&:to_h))
+            .to eq [teams[:mercedes], teams[:ferrari]].map(&:to_h)
+      end
+    end
+
+    describe '.qualifying_results' do
+      let(:url) { "http://ergast.com/api/f1/#{year}/#{round}/qualifying.json" }
+      let(:qualy_results_fixture) { "#{fixtures_path}/qualy_results.json" }
+
+      before do
+        stub_request(:get, url)
+            .to_return(status: 200, body: IO.read(qualy_results_fixture))
+      end
+
+      it 'returns the qualifying results of the given race' do
+        expect(subject.qualifying_results(year, round).map(&:to_h))
+            .to eq [qualy_results[:hamilton_australia],
+                    qualy_results[:rosberg_australia]].map(&:to_h)
+      end
+    end
+
+    describe '.race_results' do
+      let(:url) { "http://ergast.com/api/f1/#{year}/#{round}/results.json" }
+      let(:race_results_fixture) { "#{fixtures_path}/race_results.json" }
+
+      before do
+        stub_request(:get, url)
+            .to_return(status: 200, body: IO.read(race_results_fixture))
+      end
+
+      it 'returns the results of the given race' do
+        expect(subject.race_results(year, round).map(&:to_h))
+            .to eq [race_results[:hamilton_australia],
+                    race_results[:rosberg_australia]].map(&:to_h)
       end
     end
   end
