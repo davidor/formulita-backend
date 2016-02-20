24e287c5899fadefa73575a3fcc1bdcbc03a1689
David Ortiz <z.david.ortiz@gmail.com>
2016-02-20 18:07:07 +0100

    ergast client: Refactor to simplify public methods

--- lib/formulita_backend/ergast_client.rb
+++ lib/formulita_backend/ergast_client.rb
@@ -11,8 +11,52 @@ module FormulitaBackend
 
       def races(year)
         response = get(races_path(year))
-        parsed_response = parse(response)
-        races_info(parsed_response).map do |race_info|
+        parsed_response = json_response(response)
+        parsed_races(parsed_response)
+      end
+
+      # Drivers are returned in order according to their position in the
+      # championship.
+      def drivers(year)
+        response = get(drivers_path(year))
+        parsed_response = json_response(response)
+        parsed_driver_standings(parsed_response)
+      end
+
+      def teams(year)
+        response = get(teams_path(year))
+        parsed_response = json_response(response)
+        parsed_team_standings(parsed_response)
+      end
+
+      def qualifying_results(year, round)
+        response = get(qualifying_results_path(year, round))
+        parsed_response = json_response(response)
+        parsed_qualifying_info(parsed_response)
+      end
+
+      def race_results(year, round)
+        response = get(race_results_path(year, round))
+        parsed_response = json_response(response)
+        parsed_results_info(parsed_response)
+      end
+
+      private
+
+      def get(path)
+        Net::HTTP.get(URI(path))
+      end
+
+      def json_response(response)
+        JSON.parse(response)
+      end
+
+      def races_path(year)
+        "#{BASE_PATH}#{year}/races.json".freeze
+      end
+
+      def parsed_races(races_resp_json)
+        races_resp_json['MRData']['RaceTable']['Races'].map do |race_info|
           year = race_info['season'].to_i
           round = race_info['round'].to_i
           country = race_info['Circuit']['Location']['country']
@@ -21,12 +65,13 @@ module FormulitaBackend
         end
       end
 
-      # Drivers are returned in order according to their position in the
-      # championship.
-      def drivers(year)
-        response = get(drivers_path(year))
-        parsed_response = parse(response)
-        driver_standings(parsed_response).map do |driver_info|
+      def drivers_path(year)
+        "#{BASE_PATH}#{year}/driverstandings.json".freeze
+      end
+
+      def parsed_driver_standings(drivers_resp_json)
+        drivers_resp_json['MRData']['StandingsTable']['StandingsLists']
+            .first['DriverStandings'].map do |driver_info|
           first_name = driver_info['Driver']['givenName']
           last_name = driver_info['Driver']['familyName']
           nationality = driver_info['Driver']['nationality']
@@ -36,10 +81,13 @@ module FormulitaBackend
         end
       end
 
-      def teams(year)
-        response = get(teams_path(year))
-        parsed_response = parse(response)
-        team_standings(parsed_response).map do |team_info|
+      def teams_path(year)
+        "#{BASE_PATH}#{year}/constructorstandings.json".freeze
+      end
+
+      def parsed_team_standings(teams_resp_json)
+        teams_resp_json['MRData']['StandingsTable']['StandingsLists']
+            .first['ConstructorStandings'].map do |team_info|
           name = team_info['Constructor']['name']
           nationality = team_info['Constructor']['nationality']
           points = team_info['points'].to_i
@@ -47,10 +95,13 @@ module FormulitaBackend
         end
       end
 
-      def qualifying_results(year, round)
-        response = get(qualifying_results_path(year, round))
-        parsed_response = parse(response)
-        qualifying_info(parsed_response).map do |position_info|
+      def qualifying_results_path(year, round)
+        "#{BASE_PATH}#{year}/#{round}/qualifying.json".freeze
+      end
+
+      def parsed_qualifying_info(qualy_resp_json)
+        qualy_resp_json['MRData']['RaceTable']['Races']
+            .first['QualifyingResults'].map do |position_info|
           position = position_info['position'].to_i
           driver_code = position_info['Driver']['code']
           q1 = position_info['Q1']
@@ -60,10 +111,13 @@ module FormulitaBackend
         end
       end
 
-      def race_results(year, round)
-        response = get(race_results_path(year, round))
-        parsed_response = parse(response)
-        results_info(parsed_response).map do |position_info|
+      def race_results_path(year, round)
+        "#{BASE_PATH}#{year}/#{round}/results.json".freeze
+      end
+
+      def parsed_results_info(results_resp_json)
+        results_resp_json['MRData']['RaceTable']['Races']
+            .first['Results'].map do |position_info|
           position = position_info['position'].to_i
           driver_code = position_info['Driver']['code']
           laps = position_info['laps'].to_i
@@ -73,60 +127,6 @@ module FormulitaBackend
           race_points = position_info['points'].to_i
           RaceResult.new(position, driver_code, laps, time, status, grid, race_points)
         end
-      end
-
-      private
-
-      def get(path)
-        Net::HTTP.get(URI(path))
-      end
-
-      def parse(response)
-        JSON.parse(response)
-      end
-
-      def races_path(year)
-        "#{BASE_PATH}#{year}/races.json".freeze
-      end
-
-      def races_info(races_parsed_resp)
-        races_parsed_resp['MRData']['RaceTable']['Races']
-      end
-
-      def drivers_path(year)
-        "#{BASE_PATH}#{year}/driverstandings.json".freeze
-      end
-
-      def driver_standings(drivers_parsed_resp)
-        drivers_parsed_resp['MRData']['StandingsTable']['StandingsLists']
-            .first['DriverStandings']
-      end
-
-      def teams_path(year)
-        "#{BASE_PATH}#{year}/constructorstandings.json".freeze
-      end
-
-      def team_standings(teams_parsed_resp)
-        teams_parsed_resp['MRData']['StandingsTable']['StandingsLists']
-            .first['ConstructorStandings']
-      end
-
-      def qualifying_info(qualy_parsed_resp)
-        qualy_parsed_resp['MRData']['RaceTable']['Races']
-            .first['QualifyingResults']
-      end
-
-      def qualifying_results_path(year, round)
-        "#{BASE_PATH}#{year}/#{round}/qualifying.json"
-      end
-
-      def results_info(results_parsed_resp)
-        results_parsed_resp['MRData']['RaceTable']['Races']
-            .first['Results']
-      end
-
-      def race_results_path(year, round)
-        "#{BASE_PATH}#{year}/#{round}/results.json"
       end
 
     end
