8281de845a4352fc721183ffcc4c4920018480a2
David Ortiz <z.david.ortiz@gmail.com>
2016-02-27 23:51:57 +0100

    Add server

---
+++ lib/server.rb
@@ -0,0 +1,13 @@
+require 'sinatra'
+
+DATA_PATH = File.expand_path('../../data', __FILE__).freeze
+
+get '/seasons/:year' do |year|
+  content_type :json
+
+  begin
+    File.read("#{DATA_PATH}/#{year}.json")
+  rescue Errno::ENOENT
+    status(404)
+  end
+end
